ac80ff0552016e764b5778e4a8380e4e85675283
Daniel Gatti <Dan.Gatti@jax.org>
2018-02-21 09:24:17 -0500

    Adding R files

---
+++ R/get_ensembl_genes.R
@@ -0,0 +1,41 @@
+#' Get the ensembl genes on a given set of chromosomes.
+#'
+#' @param build integer that is the Ensembl build to use.
+#' @param chr character vector of chromosomes from which to get genes.
+#' @return data.frame with the genes on the given chromosomes.
+#' @examples
+#' ens = get_ensembl_genes(chr = "19", build = 90)
+get_ensembl_genes = function(chr = c(1:19, "X", "Y", "MT"), build = 90) {
+
+  if(is.null(build) || build == "") {
+    stop("Build argument must contain a valid ensembl build ID.")
+  }
+
+  if(is.null(chr) || build == "") {
+    stop("Chr argument must contain a valid chromsome ID.")
+  }
+
+  hub = query(AnnotationHub(), c("ensembl", "mus musculus", "gtf", build))
+  wh = grep(paste0("Mus_musculus.GRCm38.", build,".gtf"), hub$title)
+
+  if(length(wh) == 0) {
+    stop(paste("No Ensembl build", build, "is available from the",
+         "Bioconductor Annotation Hub."))
+  }
+
+  ensembl = hub[[names(hub)[wh]]]
+  ensembl = ensembl[ensembl$type == "gene"]
+  ensembl = ensembl[seqnames(ensembl) %in% chr]
+
+  retval = data.frame(ensembl = ensembl$gene_id,
+                      symbol  = ensembl$gene_name,
+                      chr     = seqnames(ensembl), 
+                      start   = start(ensembl),
+                      end     = end(ensembl),
+                      strand  = strand(ensembl),
+                      biotype = ensembl$gene_biotype,
+                      stringsAsFactors = FALSE)
+
+  return(retval)
+
+} # get_ensembl_genes() 
---
+++ R/mediate.R
@@ -0,0 +1,65 @@
+#' Regress one phenotype on the given genes at a single marker.
+#'
+#' @param pheno: numeric matrix with one column. Sample IDs must be in rownames and must match rownames in genoprobs.
+#' @param chr: character containing a single chromosome ID.
+#' @param pos: numeric value indicating the position in Mb at which to mediate.
+#' @param type: character value that is either 'haplo' or 'snp', indicating which type of genotype values to mediate upon.
+#' @param expr: numeric matrix containing the gene expression values. Sample names in rownames and Ensembl IDs in colnames.
+#' @param ensembl: data.frame containing gene annotation as obtained from \code{get_ensembl_gene}.
+#' @param genoprobs: list containing 3-dimensional arrays of allele probs, one for each chromosome. In qtl2 format. Samples in rows, 8 founders in columns, markers in slices.
+#' @param addcovar: numeric matrix of additive covariates without the intercept. Sample IDs must be in rownames. Typically creates using model.matrix(~covar1 + covar2, data = data)[,-1].
+#' @param cc_dbfile: full path to the CC_SNP database file created by Karl Broman.
+#' @return data.frame containing the mediation results.
+mediate = function(pheno, chr, pos, type = c("haplo", "snp"), expr, ensembl,
+          genoprobs, addcovar, cc_dbfile) {
+
+  type = match.arg(type)
+
+  query_fxn = create_variant_query_func(cc_dbfile, filter = "type=='snp'")
+
+  # Get the genotype (Q).
+  # NOTE: we remove the first column of genoprobs in the haplo probs case.
+  Q = switch(type,
+         haplo = get_allele_probs(chr = chr, pos = pos, genoprobs = genoprobs,
+                 map = map)[,-1],
+         snp = get_snp_probs(chr = chr, pos = pos, genoprobs = genoprobs,
+                   map = map, query_fxn = query_fxn))
+
+  Q = data.frame(mouse = rownames(Q), Q)
+
+  # Set up tow models to compare.
+  full_mod = function(df) { as.numeric(logLik(lm(pheno ~ ., data = df))) }
+  red_mod  = function(df) { as.numeric(logLik(lm(pheno ~ ., data = select(df, -Q)))) }
+  if(type == "haplo") {
+    red_mod = function(df) { as.numeric(logLik(lm(pheno ~ ., data = select(df, -(B:H))))) }
+  }
+
+  pheno.df = data.frame(mouse = rownames(pheno), pheno = pheno[,1])
+  common.genes = intersect(colnames(expr), ensembl$ensembl)
+  expr = expr[,common.genes]
+  ensembl = ensembl[match(common.genes, ensembl$ensembl),]
+  all(colnames(expr) == ensembl$ensembl)
+  expr.df  = data.frame(mouse = rownames(expr), expr)
+  addcovar.df = data.frame(mouse = rownames(addcovar), addcovar)
+
+  df = left_join(pheno.df, Q,  by = "mouse") %>%
+           left_join(y = addcovar.df, by = "mouse") %>%
+           left_join(y = expr.df, by = "mouse") %>%
+           select(-mouse) %>%
+           gather_("ensembl", "expr", colnames(.)[grep("^ENSMUSG", colnames(.))]) %>%
+           group_by(ensembl) %>%
+           nest()
+
+  df = df %>% 
+           mutate(full_ll = map_dbl(data, full_mod),
+                  red_ll  = map_dbl(data, red_mod)) %>%
+                  select(ensembl, full_ll, red_ll) %>%
+                  mutate(lod = (full_ll - red_ll) / log(10))
+
+  df = left_join(df, select(ensembl, ensembl:start), by = "ensembl") %>%
+       mutate(start = start * 1e-6)
+
+  return(df)
+
+} # mediate()
+
